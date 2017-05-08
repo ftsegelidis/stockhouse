ea49e6b4c86368c3887da10a97b7bcfcc9579978
ftsegelidis <ftsegelidis@gmail.com>
2017-05-08 09:06:15 +0300

    adding basic screen for actions

--- android app/app/src/main/java/com/androidcss/searchexample/startScreen.java
+++ android app/app/src/main/java/com/androidcss/searchexample/startScreen.java
@@ -1,7 +1,9 @@
 package com.androidcss.searchexample;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class startScreen extends AppCompatActivity {
 
@@ -10,4 +12,22 @@ public class startScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_screen);
     }
+
+    public void startSearchActivity(View v) {
+        Intent intent = new Intent(startScreen.this, SearchActivity.class);
+        startActivity(intent);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
